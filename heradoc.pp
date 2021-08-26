4a0a75f42d69f4b59d8295f87830dc41228510e9
John Bond <github@johnbond.org>
2021-08-26 18:01:21 +0200

    add reduce example

--- heradoc.pp
+++ heradoc.pp
@@ -1,8 +1,24 @@
+define foo (
+  #Stdlib::Unixpath $command,
+  Pattern[/\A(.+)*\z/] $command,
+) {
+  notice($command)
+}
 $foo = 'test'
 $bar = @('BAR')
         adasd
         ${foo}
         asd
     | 'BAR'
-notify{$bar:}
+#notify{$bar:}
+$output_dir = '/foo'
+$large_cleanup_cmd = @("CLEANUP_CMD")
+/usr/bin/find ${output_dir} -mindepth 1 -maxdepth 1 -type d -ctime +7 -exec du -ks {} + | \
+  awk '\$1 >= 1000000 {print \$2}' | \
+  xargs rm -rf
+| CLEANUP_CMD
+foo {'test':
+  command => $large_cleanup_cmd,
+}
 
+#notice(inline_template('<%= @large_cleanup_cmd %>'))
--- reduce.pp
+++ reduce.pp
@@ -9,10 +9,27 @@
 #$bar = [' *', '..*'].reduce("${cmd} ${mypath}") |$memo, $value| {
 #    "${memo}, !${cmd} ${mypath}${value}"
 #}
-$foo = {'valid'  => true, 'false' => false, 'undef' => undef}
-$bar = $foo.reduce({}) |$memo, $value| {
-    unless $value[1] == undef {
-        merge($memo, {$value[0] => $value[1]})
-    } else { $memo }
+#$foo = {'valid'  => true, 'false' => false, 'undef' => undef}
+#$bar = $foo.reduce({}) |$memo, $value| {
+#    unless $value[1] == undef {
+#        merge($memo, {$value[0] => $value[1]})
+#    } else { $memo }
+#}
+#notice($bar)
+$d = {'sda' => 'ssd', 'sdb' => 'ssd', 'sdc' => 'hdd'}
+$type_count = $d.reduce({'ssd'=> 0, 'hdd' => 0}) |$memo, $value| {
+  if $value[1] == 'ssd'{
+    $ssd = $memo['ssd'] + 1
+    $hdd = $memo['hdd']
+  } else {
+    $ssd = $memo['ssd']
+    $hdd = $memo['hdd'] + 1
+  }
+  $m = {
+    'hdd' => $hdd,
+    'ssd'  => $ssd,
+  }
+  $m
 }
-notice($bar)
+$type = ($type_count['ssd'] > $type_count['hdd']).bool2str('ssd', 'hdd')
+notice($type)
